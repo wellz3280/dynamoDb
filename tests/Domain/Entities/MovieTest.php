2dcb27a92a78698c0594d83a38c92e103372b2cc
weliton <welingtonzsilva@gmail.com>
2022-06-06 21:19:29 -0300

    criado testes para a classe movie

---
+++ tests/Domain/Entities/MovieTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain\Entities;
+
+use AwsDynamoDb\Domain\Entities\Movie;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers AwsDynamoDb\Domain\Entities\Movie
+ */
+final class MovieTest extends TestCase
+{
+    /**
+     * Should Instantiate Movie Entity
+     * @dataProvider movieProvider
+     * @test
+     */
+    public function ShouldInstantiateMovieEntity(int $id, string $category, string $title, string $caption): void
+    {
+        $movie = new Movie($id, $category, $title, $caption);
+
+        self::assertSame($id, $movie->getId());
+        self::assertSame($category, $movie->getCategory());
+        self::assertSame($title, $movie->getTitle());
+        self::assertSame($caption, $movie->getCaption());
+    }
+
+    public function movieProvider(): array
+    {
+    return [
+            [1,'ação','avatar','pandora'],
+        ];
+    }
+}
